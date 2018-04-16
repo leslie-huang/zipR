e8e59afa95370a2b49656d91767cc5e7d764fafe
leslie-huang <lesliehuang@gmail.com>
2018-04-16 16:59:39 -0400

    add functionality to fill in shorter sequence with val(s)

---
+++ R/fill.R
@@ -0,0 +1,15 @@
+#'
+#' Use a given fillvalue to fill in a shorter sequence and returns a sequence of equal length to the longer sequence. Takes a subset of the fill sequence if fill sequence is longer than the difference between the longer and shorter sequences
+#' @param shorter shorter vector-like object
+#' @param longer longer vector-like object
+#' @param fillvalue sequence of value(s) to fill in shorter vector. If fillvalue is longer than the difference between `shorter` and `longer`, values from fillvalue will be taken only until `shorter` is the same length as `longer`
+#' 
+
+fill <- function(shorter, longer, fillvalue) {
+  diff <- length(longer) - length(shorter)
+  
+  fillers <- rep(fillvalue, diff)[1:diff]
+  shorter <- c(shorter, fillers)
+  
+  return(shorter)
+}
--- R/zip.R
+++ R/zip.R
@@ -3,19 +3,38 @@
 #' @param x vector-like object
 #' @param y vector-like object
 #' @param broadcast defaults to FALSE; if TRUE, shorter sequence is repeated until its length is equal to that of the longer sequence
+#' @param fillvalue value or sequence of value(s) to fill in shorter sequence until it is the same length as the longer sequence
 
-zip <- function(x, y, broadcast = FALSE) {
+zip <- function(x, y, broadcast = FALSE, fillvalue = NA) {
   if (broadcast == FALSE) {
-    check_length(x, y)
+    if (fillvalue == NA) {
+      check_length(x, y)
+    }
+    else { # broadcast = FALSE but fillvalue = some sequence
+      if (length(x) > length(y)) {
+        y <- fill(x, y)
+      }
+      else {
+        x <- fill(y, x)
+      }
+    }
   }
   
-  else {
-    if (length(x) > length(y)) {
-      y <- broadcast(x, y)
+  else if (broadcast == TRUE) {
+    if (fillvalue != NA) {
+      stop("Cannot specify both broadcast = TRUE and fillvalue = seq ")
     }
-    else {
-      x <- broadcast(y, x)
+    
+    else { # fillvalue = NA
+      if (length(x) > length(y)) {
+        y <- broadcast(x, y)
+      }
+      
+      else {
+        x <- broadcast(y, x)
+      }
     }
+    
   }
   
   result <- data.frame(x = x, 
