c8c35e79dbf3c011865d0eb9295891ddf8e42113
leslie-huang <lesliehuang@gmail.com>
2018-04-16 17:07:22 -0400

    fixed bug

--- R/zip.R
+++ R/zip.R
@@ -4,40 +4,41 @@
 #' @param y vector-like object
 #' @param broadcast defaults to FALSE; if TRUE, shorter sequence is repeated until its length is equal to that of the longer sequence
 #' @param fillvalue value or sequence of value(s) to fill in shorter sequence until it is the same length as the longer sequence
+#' 
+
 
 zip <- function(x, y, broadcast = FALSE, fillvalue = NA) {
-  if (broadcast == FALSE) {
-    if (fillvalue == NA) {
-      check_length(x, y)
+  if (broadcast == TRUE & fillvalue != NA) {
+    stop("Error: cannot specify both broadcast = TRUE and fillvalue = seq ")
+  }
+  
+  else if (broadcast == FALSE & fillvalue == NA) {
+    check_length(x, y)
+    
+  }
+  
+  else if (broadcast == TRUE & fillvalue == NA) {
+    if (length(x) > length(y)) {
+      y <- broadcast(x, y)
     }
-    else { # broadcast = FALSE but fillvalue = some sequence
-      if (length(x) > length(y)) {
-        y <- fill(x, y)
-      }
-      else {
-        x <- fill(y, x)
-      }
+    
+    else {
+      x <- broadcast(y, x)
     }
   }
   
-  else if (broadcast == TRUE) {
-    if (fillvalue != NA) {
-      stop("Cannot specify both broadcast = TRUE and fillvalue = seq ")
+  else if (broadcast == FALSE & fillvalue != NA) {
+    if (length(x) > length(y)) {
+      y <- fill(x, y)
     }
-    
-    else { # fillvalue = NA
-      if (length(x) > length(y)) {
-        y <- broadcast(x, y)
-      }
-      
-      else {
-        x <- broadcast(y, x)
-      }
+    else {
+      x <- fill(y, x)
     }
-    
   }
+  
   
   result <- data.frame(x = x, 
                        y = y)
   return(result)
+  
 }
